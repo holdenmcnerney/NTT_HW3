206f0ab122e0f3f5ba23f5732d798d09fd5fdb41
holdenmcnerney <holdenmcnerney@gmail.com>
2023-10-21 21:36:30 -0400

    test not related to code

--- main.py
+++ main.py
@@ -8,11 +8,8 @@ def main():
     gps_data = np.genfromtxt('gps.txt', delimiter=',', dtype=float)
     imu_data = np.genfromtxt('imu.txt', delimiter=',', dtype=float)
 
-    print(gps_data[0])
-    print(gps_data[1])
-    print(gps_data[2])
-
-    pass
+    
+    
 
     return 1
 
